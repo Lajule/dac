6cefbf541a033572a6591335ebfd277ee787b0ce
Julien ROUZIERES <julien.rouzieres@bjtmail.com>
2023-05-15 22:10:57 +0200

    Feat: add save

--- cmd/root.go
+++ cmd/root.go
@@ -2,19 +2,19 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"log"
 	"os"
 
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-	_ "github.com/mattn/go-sqlite3"
 	"github.com/Lajule/dac/app"
 	"github.com/Lajule/dac/ent"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/spf13/cobra"
 )
 
 var (
-	cfgFile string
+	dbFile string
 
 	rootCmd = &cobra.Command{
 		Use:   "dac",
@@ -62,24 +62,30 @@ var (
 			}
 
 			d.Start()
-			log.Println("training session: ", d)
 
-			client, err := ent.Open("sqlite3", "file:dac.db?cache=shared&_fk=1")
+			save, err := cmd.Flags().GetBool("close")
 			if err != nil {
-				log.Fatalf("failed opening connection to sqlite: %v", err)
-			}
-			defer client.Close()
-			if err := client.Schema.Create(context.Background()); err != nil {
-				log.Fatalf("failed creating schema resources: %v", err)
-			}
-			t, err := client.Training.
-				Create().
-				Save(context.Background())
-			if err != nil {
-				log.Fatalf("failed creating user: %v", err)
+				log.Fatalf("failed getting save: %v", err)
 			}
 
-			log.Println("training was created: ", t)
+			if save {
+				client, err := ent.Open("sqlite3", fmt.Sprintf("file:%s?cache=shared&_fk=1", dbFile))
+				if err != nil {
+					log.Fatalf("failed opening connection to sqlite: %v", err)
+				}
+				defer client.Close()
+				if err := client.Schema.Create(context.Background()); err != nil {
+					log.Fatalf("failed creating schema resources: %v", err)
+				}
+				if _, err := client.Training.
+					Create().
+					SetDuration(int(d.Duration)).
+					SetSpeed(d.Speed).
+					SetPrecision(d.Precision).
+					Save(context.Background()); err != nil {
+					log.Fatalf("failed creating training: %v", err)
+				}
+			}
 		},
 	}
 )
@@ -91,28 +97,8 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.dac.yaml)")
+	rootCmd.PersistentFlags().StringVar(&dbFile, "db", "dac.db", "Database file (default is dac.db)")
 	rootCmd.Flags().DurationP("duration", "d", 0, "Duration of the training session")
 	rootCmd.Flags().BoolP("close", "c", false, "Close on session timeout")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		viper.SetConfigFile(cfgFile)
-	} else {
-
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".dac")
-	}
-
-	viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err == nil {
-		log.Println("using config file: ", viper.ConfigFileUsed())
-	}
+	rootCmd.Flags().BoolP("save", "s", true, "Save session in database")
 }
